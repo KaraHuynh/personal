2e24c3a60d06f77d6616c18fe3981c9a5dc9c225
Kara Huynh <karahuynh122@gmail.com>
2022-07-13 15:55:58 -0400

    setting up the changing status; must make an animation for it

--- pages/welcome.js
+++ pages/welcome.js
@@ -1,6 +1,26 @@
 import Image from 'next/image'
+import React, { useState, useEffect } from "react";
+
 export default function Welcome(){
-    const things = ["incoming third year computer science student...", "an aspiring software developer...", "a passionate gamer..."];
+    const [status, updateStatus] = useState(["incoming third year computer science student...", "an aspiring software developer...", "a passionate gamer..."]);
+    const [displayElement, updateElement] = useState("");
+
+    let index = 0;
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            updateElement(status[index]);
+            index += 1;
+            if (index >= status.length) {
+                index = 0
+            }
+            }, 3000);
+
+            return () => {
+            clearInterval(interval);
+            };
+    }, []);
+    
     return(
         <div className="mt-20 flex flex-col md:flex-row">
             <div className="flex-1">
@@ -12,9 +32,9 @@ export default function Welcome(){
                 />
             </div>
             <div className="flex-1 ml-10 mt-40">
-                <h1 className="text-5xl text-white">hi, my name is kara</h1>
+                <h1 className="text-4xl lg:text-5xl text-white">hi, my name is kara</h1>
                 <hr className="underline mt-2 mb-2"></hr>
-                {}
+                <h1 className="text-4xl lg:text-5xl text-white">{displayElement}</h1>
             </div>
 
         </div>
