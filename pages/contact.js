51cb2f7034c0484d0ccfe5c8c3cbb2e4d412be6f
Kara Huynh <karahuynh122@gmail.com>
2022-07-14 15:08:00 -0400

    updating form

--- pages/contact.js
+++ pages/contact.js
@@ -10,14 +10,24 @@ export default function Contact(){
 
                 <form id="contact-form" name="contact-form" encType="text/plain" action="https://docs.google.com/forms/u/0/d/e/1FAIpQLScZh51tscFeIOnYV_ucGtjZkwN68-du-htEkOGBkHPyR06kAA/formResponse" target="hidden_iframe" onSubmit="submitted=true;">
                     <div className="mb-3">
-                        <input type="email" id="email" className=" text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="your name" required></input>
+                        <input type="text" className=" text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name" required></input>
+                    </div>
+                    <div className="mb-3">
+                        <input type="email" id="email" className=" text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="email" required></input>
+                    </div>
+                    <div className="mb-3">
+                        <input type="email" id="email" className=" text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="email" required></input>
+                    </div>
+                    <div className="buttonHolder mb-10">
+                        <button type="submit" value="Submit">
+                            <img src="https://cdn.discordapp.com/attachments/838956183850844190/997214985585434624/Paper_Plane.png" width="50"/>
+                        </button>
                     </div>
                     
-                    <button type="submit" value="Submit">Submit</button>
                 </form>
             
                 <div className="contact-icons flex flex-row justify-center pb-10">
-                    <a href="https://github.com/KaraHuynh" className="hover:opacity-75 transition-all">
+                    <a href="https://github.com/KaraHuynh" className="hover:opacity-75 transition-all mr-3">
                         <img 
                             src="https://cdn.discordapp.com/attachments/878469526117810217/996945944153174066/github.png"
                             alt="github"
@@ -31,7 +41,7 @@ export default function Contact(){
                             width="60"
                         />
                     </a>
-                    <a href="https://devpost.com/kara-huynh?ref_content=user-portfolio&ref_feature=portfolio&ref_medium=global-nav" className="hover:opacity-75 transition-all">
+                    <a href="https://devpost.com/kara-huynh?ref_content=user-portfolio&ref_feature=portfolio&ref_medium=global-nav" className="hover:opacity-75 transition-all ml-3">
                         <img 
                             src="https://cdn.discordapp.com/attachments/878469526117810217/996945888557662238/web.png"
                             alt="devpost"
