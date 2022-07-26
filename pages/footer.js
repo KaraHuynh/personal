fae0a65dd32fa3eb711f13089e4f4d6528888fb4
Kara Huynh <karahuynh122@gmail.com>
2022-07-26 18:34:40 -0400

    favicon

--- pages/footer.js
+++ pages/footer.js
@@ -1,7 +1,7 @@
 export default function Footer(){
     return(
         <h1 className="text-2xl text-white text-center mt-40 mb-5">
-            made with love ♡ kara huynh 2022 &copy;
+            &copy; made with love ♡ kara huynh 2022 
         </h1>
     )
 }
