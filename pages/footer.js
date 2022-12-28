e42317b04fd1b4e0be3c73f1a1be1930486356d4
Kara Huynh <karahuynh122@gmail.com>
2022-12-27 21:43:05 -0500

    [UPDATE]: fixed smaller screen fonts

--- pages/footer.js
+++ pages/footer.js
@@ -1,6 +1,6 @@
 export default function Footer(){
     return(
-        <h1 className="text-xl md:text-2xl text-white text-center mt-40 mb-5">
+        <h1 className="text-lg md:text-2xl text-white text-center mt-40 mb-5">
             &copy; kara huynh 2022 ♡ made with love 
         </h1>
     )
